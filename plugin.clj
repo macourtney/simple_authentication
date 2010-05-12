864212e9fbc1ab4effe4a549c6404a9a57931410
Matt Courtney <macourtney@gmail.com>
2010-05-12 11:07:17 -0400

    Install will now create a migration for the users table.

--- plugin.clj
+++ plugin.clj
@@ -1,7 +1,24 @@
-(ns plugins.simple-authentication.plugin)
+(ns plugins.simple-authentication.plugin
+  (:require [conjure.model.util :as model-util]
+            [destroyers.migration-destroyer :as migration-destroyer]
+            [generators.migration-generator :as migration-generator]))
 
-(defn install [arguments])
+(def model-name "user")
+(def migration-name "create-users")
 
-(defn uninstall [arguments])
+(defn migration-up-content []
+  (str "(create-table \"" (model-util/model-to-table-name model-name) "\" 
+    (id)
+    (string :name)
+    (string :password))"))
+
+(defn migration-down-content []
+  (str "(drop-table \"" (model-util/model-to-table-name model-name) "\")"))
+
+(defn install [arguments]
+  (migration-generator/generate-migration-file migration-name (migration-up-content) (migration-down-content)))
+
+(defn uninstall [arguments]
+  (migration-destroyer/destroy-migration-file migration-name))
 
 (defn initialize [])
