78edff3b5d3c9d811feed214e8590a98e775adaa
Matt Courtney <macourtney@gmail.com>
2010-07-09 12:55:15 -0400

    Updated to release candidate 1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-authentication "1.0.0-SNAPSHOT"
+(defproject simple-authentication "1.0.0-RC1"
   :description "This is a simple authentication plugin for Conjure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
