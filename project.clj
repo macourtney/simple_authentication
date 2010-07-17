97d875810c3a583cdefcb873e0530d33ad09f0b9
Matthew Courtney <mcourtne199@yahoo.com>
2010-07-17 07:49:52 -0400

    Updated the version to 1.0.0, and updated the readme file to reference Leiningen.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject simple-authentication "1.0.0-RC1"
+(defproject simple-authentication "1.0.0"
   :description "This is a simple authentication plugin for Conjure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [conjure-script "0.7.0-RC2"]]
+                 [conjure-script "0.7.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
