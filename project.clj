b7c3b6cdfc53c0cf6a5b4bb402de8d1528947519
Matt Courtney <macourtney@gmail.com>
2010-09-30 10:16:05 -0400

    Updated the version to 1.1.0-RC1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject simple-authentication "1.1.0-SNAPSHOT"
+(defproject simple-authentication "1.1.0-RC1"
   :description "This is a simple authentication plugin for Conjure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [conjure-script "0.8.0-RC1"]]
+                 [conjure-script "0.8.0-RC2"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
